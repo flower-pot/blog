150a074471d7f9e3be760e4927206b4c7d923341
flower-pot <flowerpot@pondati.net>
2014-03-10 19:05:41 +0100

    change style of github repo plugin

--- _plugins/github_repo_tag.rb
+++ _plugins/github_repo_tag.rb
@@ -13,7 +13,7 @@ module Jekyll
     def render(context)
       result = "<div class='github-repo clearfix panel panel-default' style='width: 250px;'>"
       result << "  <div class='panel-body'>"
-      result << "    <p class='github-repo-name'><i style='font-size: 120%' class='fa fa-github'> </i> <a href='#{@repo["html_url"]}'>#{@repo["full_name"]}</a> - #{@repo["language"]}</p>"
+      result << "    <p class='github-repo-name'><i style='font-size: 120%' class='fa fa-github'> </i> <a href='#{@repo["html_url"]}'>#{@repo["full_name"]}</a> (#{@repo["language"]})</p>"
       result << "    <p class='github-repo-description'>#{@repo["description"]}</p>"
       result << "    <p><a class='btn btn-default pull-left' href='#{@repo["html_url"]}'>Fork <span class='label label-primary'>#{@repo["forks_count"]}</span></a><a class='btn btn-default pull-right' href='#{@repo["html_url"]}'>Watch <span class='label label-primary'>#{@repo["watchers_count"]}</span></a></p>"
       result << "  </div>"
